0d4146f55ff3cb2e5a8ec493f343442ddd8a9550
degrandelucas <lucasdegrande15@gmail.com>
2024-12-12 19:12:45 -0300

    codigo arvore binaria automatica implatanda

--- src/App.java
+++ src/App.java
@@ -8,17 +8,33 @@ public class App {
     public static void main(String[] args) throws FileNotFoundException {
         Set<Integer> listaNumeros = new TreeSet<>();
         Random generator = new Random();
-        int numeroProcurado = 28;
+        int chosenNumber = 28;
+        int iterations = 0;
+        int size = 128;
+        int less = 0;
+        int major = size;
+        int middle = 0;
 
-        do  {
+        do {
             listaNumeros.add(generator.nextInt(129));
-        } while (listaNumeros.size() < 128);
+        } while (listaNumeros.size() < size);
 
         System.out.println(listaNumeros);
 
-        
+        int shot = generator.nextInt(129);
+        do {
+            middle = less + (major - less) / 2;
+            if (middle == shot) {
+                break;
+            } else if (middle < shot) {
+                less = middle + 1;
+            } else { // middle > shot
+                major = middle - 1;
+            }
+            iterations++;
+        } while (chosenNumber != shot);
+
+        System.out.println("Foram realizadas " + iterations + " iteracoes para encontrar o numero sorteador " + shot);
 
     }
-
-
 }
