7f389dcf074075a645713f91f66e20dde8304270
degrandelucas <lucasdegrande15@gmail.com>
2024-12-12 17:55:40 -0300

    gerando numeros atraves do metodo ramdon, para criar a lista dos numeros

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.random.RandomGenerator;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Set<Integer> listaNumeros = new TreeSet<>();
+        Random generator = new Random();
+        int numeroProcurado = 28;
+
+        do  {
+            listaNumeros.add(generator.nextInt(129));
+        } while (listaNumeros.size() < 128);
+
+        System.out.println(listaNumeros);
+
+        
+
     }
+
+
 }
